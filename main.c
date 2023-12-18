09ac4454fc14c36bd0ff73afcf7c5a6f9db91f71
Grzegorz121979 <pociechagrzegorz@outlook.com>
2023-12-18 20:21:44 +0100

    next commit

--- main.c
+++ main.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 
-char* greater_less(int x, int y)
-{
-  if (x > y)
-  {
-    return "x is greater then y\n";
-  }
-  else if (x < y)
-  {
-    return "x is less then y\n";
-  }
-  else
-  {
-    return "x is equal y\n";
-  }
-}
-
 int main(void)
 {
-  int x;
-  int y;
+  char c;
+  printf("Do you agree? ");
+  scanf("%c", &c);
 
-  printf("X: ");
-  scanf("%d", &x);
-  printf("Y: ");
-  scanf("%d", &y);
-
-  char* answer = greater_less(x, y);
-
-  printf(answer);
+  if (c == 'y' || c == 'Y')
+  {
+      printf("Agreed\n");
+  }
+  else if (c == 'n' || c == 'N')
+  {
+    printf("Not agreed\n");
+  }
 
   return 0;
 }
